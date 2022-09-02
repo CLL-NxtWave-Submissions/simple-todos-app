f89e14fb9042fc13e21ab4adb5a7aa88fc4adb0e
Surya T N <suryatej.nim@yahoo.com>
2022-09-02 17:51:38 +0000

    Implemented TodoItem component in the respective index.js, index.css files

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,1 +1,20 @@
-// Write your code here
+import './index.css'
+
+const TodoItem = props => {
+  const {title, deleteTodoItemHandler} = props
+
+  return (
+    <li className="todo-item-container">
+      <p className="todo-item-title">{title}</p>
+      <button
+        type="button"
+        className="todo-item-delete-button"
+        onClick={deleteTodoItemHandler}
+      >
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
